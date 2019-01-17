3753a56be3bc22324e5ad2a53c08febac5917f7d
Alex Samland <alexandersamland2020@u.northwestern.edu>
2019-01-17 15:20:56 -0600

    calibration and filter fixed

--- week1_student.cpp
+++ week1_student.cpp
@@ -31,7 +31,7 @@ enum Ascale {
   AFS_8G,
   AFS_16G
 };
- 
+
 enum Gscale {
   GFS_250DPS = 0,
   GFS_500DPS,
@@ -39,17 +39,9 @@ enum Gscale {
   GFS_2000DPS
 };
 
-struct Keyboard {
-  char key_press;
-  int heartbeat;
-  int version;
-};
-Keyboard* shared_memory; 
-int run_program=1;
- 
 int setup_imu();
-void calibrate_imu();      
-void read_imu();    
+void calibrate_imu();
+void read_imu();
 void update_filter();
 
 //global variables
@@ -60,7 +52,7 @@ float z_gyro_calibration=0;
 float roll_calibration=0;
 float pitch_calibration=0;
 float accel_z_calibration=0;
-float imu_data[6]; //gyro r p y, accel r p z
+float imu_data[6]; //gyro xyz, accel xyz
 long time_curr;
 long time_prev;
 struct timespec te;
@@ -71,27 +63,27 @@ float gyro_pitch = 0;
 float pitch_angle=0;
 float roll_angle=0;
 
- 
+
 int main (int argc, char *argv[])
 {
 
     setup_imu();
     calibrate_imu();
+    //read_imu();
+    //printf("imu read: %f %f %f %f %f %f\n\r", imu_data[0], imu_data[1], imu_data[2], imu_data[3], imu_data[4],imu_data[5]);
 
-    printf("imu read: %f %f %f %f %f %f\n\r", imu_data[0], imu_data[1], imu_data[2], imu_data[3], imu_data[4],imu_data[5]);
-    
     while(1)
     {
       read_imu();
-      update_filter();   
+      update_filter();
       printf("%f\t %f\t %f\t %f\t %f\t %f\n\r", roll_angle, imu_data[3], gyro_roll, pitch_angle, imu_data[4], gyro_pitch);
 
-     
+
     }
-      
-    
-   
-  
+
+
+
+
 }
 
 void calibrate_imu()
@@ -116,7 +108,7 @@ void calibrate_imu()
 
   count++;
  }
-  
+
   x_gyro_calibration=xtemp;
   y_gyro_calibration=ytemp;
   z_gyro_calibration=ztemp;
@@ -125,8 +117,8 @@ void calibrate_imu()
   accel_z_calibration=zacctemp;
 
 
-//print calibration data to screen  
-//printf("calibration complete, %f %f %f %f %f %f\n\r",x_gyro_calibration,y_gyro_calibration,z_gyro_calibration,roll_calibration,pitch_calibration,accel_z_calibration);
+//print calibration data to screen
+printf("calibration complete, %f %f %f %f %f %f\n\r",x_gyro_calibration,y_gyro_calibration,z_gyro_calibration,roll_calibration,pitch_calibration,accel_z_calibration);
 
 
 }
@@ -136,9 +128,9 @@ void read_imu()
   int address=59;
   float ax=0;
   float az=0;
-  float ay=0; 
+  float ay=0;
   int vh,vl;
-  
+
   //read in data
   vh=wiringPiI2CReadReg8(imu,address);
   vl=wiringPiI2CReadReg8(imu,address+1);
@@ -148,10 +140,10 @@ void read_imu()
   {
     vw=vw ^ 0xffff;
     vw=-vw-1;
-  }          
+  }
   float xacc=vw/16384.;//  DONE: convert vw from raw values to "g's"
-  
-  
+
+
   address=61;
   vh=wiringPiI2CReadReg8(imu,address);
   vl=wiringPiI2CReadReg8(imu,address+1);
@@ -160,10 +152,10 @@ void read_imu()
   {
     vw=vw ^ 0xffff;
     vw=-vw-1;
-  }          
+  }
   float yacc =vw/16384.; //DONE: convert vw from raw valeus to "g's"
-  
-  
+
+
   address=63;
   vh=wiringPiI2CReadReg8(imu,address);
   vl=wiringPiI2CReadReg8(imu,address+1);
@@ -172,12 +164,12 @@ void read_imu()
   {
     vw=vw ^ 0xffff;
     vw=-vw-1;
-  }        
+  }
   imu_data[5]=-vw/16384.;
-  
-  
+
+
   //roll
-  imu_data[3]=-(atan2(yacc,imu_data[5])*57.29578 - roll_calibration);
+  imu_data[3]=-(atan2(yacc,imu_data[5])*57.29578) - roll_calibration;
   //pitch
   imu_data[4]=atan2(xacc,imu_data[5])*57.29578 - pitch_calibration;
 
@@ -191,9 +183,9 @@ void read_imu()
   {
     vw=vw ^ 0xffff;
     vw=-vw-1;
-  }          
+  }
   imu_data[0]=-x_gyro_calibration+vw*0.015259;
-  
+
   address=69;
   vh=wiringPiI2CReadReg8(imu,address);
   vl=wiringPiI2CReadReg8(imu,address+1);
@@ -202,9 +194,9 @@ void read_imu()
   {
     vw=vw ^ 0xffff;
     vw=-vw-1;
-  }          
+  }
  imu_data[1]=-y_gyro_calibration+vw*0.015259;
-  
+
   address=71;
   vh=wiringPiI2CReadReg8(imu,address);
   vl=wiringPiI2CReadReg8(imu,address+1);
@@ -213,10 +205,10 @@ void read_imu()
   {
     vw=vw ^ 0xffff;
     vw=-vw-1;
-  }          
+  }
   imu_data[2]=-z_gyro_calibration+vw*0.015259;
-  
- 
+
+
 
 
 }
@@ -228,8 +220,8 @@ void update_filter()
   timespec_get(&te,TIME_UTC);
   time_curr=te.tv_nsec;
   //compute time since last execution
-  float imu_diff=time_curr-time_prev;           
-  
+  float imu_diff=time_curr-time_prev;
+
   //check for rollover
   if(imu_diff<=0)
   {
@@ -238,8 +230,8 @@ void update_filter()
   //convert to seconds
   imu_diff=imu_diff/1000000000;
   time_prev=time_curr;
-  
-  //comp. filter for roll, pitch here: 
+
+  //comp. filter for roll, pitch here:
   float roll_gyro_delta = imu_data[0]*imu_diff;
   float pitch_gyro_delta = imu_data[1]*imu_diff;
   gyro_roll += roll_gyro_delta;
@@ -257,11 +249,11 @@ void update_filter()
 int setup_imu()
 {
   wiringPiSetup ();
-  
-  
+
+
   //setup imu on I2C
   imu=wiringPiI2CSetup (0x68) ; //accel/gyro address
-  
+
   if(imu==-1)
   {
     printf("-----cant connect to I2C device %d --------\n",imu);
@@ -269,65 +261,31 @@ int setup_imu()
   }
   else
   {
-  
+
     printf("connected to i2c device %d\n",imu);
     printf("imu who am i is %d \n",wiringPiI2CReadReg8(imu,0x75));
-    
+
     uint8_t Ascale = AFS_2G;     // AFS_2G, AFS_4G, AFS_8G, AFS_16G
     uint8_t Gscale = GFS_500DPS; // GFS_250DPS, GFS_500DPS, GFS_1000DPS, GFS_2000DPS
-    
-    
+
+
     //init imu
     wiringPiI2CWriteReg8(imu,PWR_MGMT_1, 0x00);
     printf("                    \n\r");
     wiringPiI2CWriteReg8(imu,PWR_MGMT_1, 0x01);
-    wiringPiI2CWriteReg8(imu, CONFIG, 0x00);  
-    wiringPiI2CWriteReg8(imu, SMPLRT_DIV, 0x00); //0x04        
+    wiringPiI2CWriteReg8(imu, CONFIG, 0x00);
+    wiringPiI2CWriteReg8(imu, SMPLRT_DIV, 0x00); //0x04
     int c=wiringPiI2CReadReg8(imu,  GYRO_CONFIG);
     wiringPiI2CWriteReg8(imu,  GYRO_CONFIG, c & ~0xE0);
     wiringPiI2CWriteReg8(imu, GYRO_CONFIG, c & ~0x18);
-    wiringPiI2CWriteReg8(imu, GYRO_CONFIG, c | Gscale << 3);       
+    wiringPiI2CWriteReg8(imu, GYRO_CONFIG, c | Gscale << 3);
     c=wiringPiI2CReadReg8(imu, ACCEL_CONFIG);
-    wiringPiI2CWriteReg8(imu,  ACCEL_CONFIG, c & ~0xE0); // Clear self-test bits [7:5] 
+    wiringPiI2CWriteReg8(imu,  ACCEL_CONFIG, c & ~0xE0); // Clear self-test bits [7:5]
     wiringPiI2CWriteReg8(imu,  ACCEL_CONFIG, c & ~0x18); // Clear AFS bits [4:3]
-    wiringPiI2CWriteReg8(imu,  ACCEL_CONFIG, c | Ascale << 3);      
-    c=wiringPiI2CReadReg8(imu, ACCEL_CONFIG2);         
+    wiringPiI2CWriteReg8(imu,  ACCEL_CONFIG, c | Ascale << 3);
+    c=wiringPiI2CReadReg8(imu, ACCEL_CONFIG2);
     wiringPiI2CWriteReg8(imu,  ACCEL_CONFIG2, c & ~0x0F); //
     wiringPiI2CWriteReg8(imu,  ACCEL_CONFIG2,  c | 0x00);
   }
   return 0;
-}
-
-//when cntrl+c pressed, kill motors
-void trap(int signal)
-
-{
-
-   
- 
-   printf("ending program\n\r");
-   run_program=0;
-}
-
-void setup_keyboard()
-{
-
-  int segment_id;   
-  struct shmid_ds shmbuffer; 
-  int segment_size; 
-  const int shared_segment_size = 0x6400; 
-  int smhkey=33222;
-  
-  /* Allocate a shared memory segment.  */ 
-  segment_id = shmget (smhkey, shared_segment_size,IPC_CREAT | 0666); 
-  /* Attach the shared memory segment.  */ 
-  shared_memory = (Keyboard*) shmat (segment_id, 0, 0); 
-  printf ("shared memory attached at address %p\n", shared_memory); 
-  /* Determine the segment's size. */ 
-  shmctl (segment_id, IPC_STAT, &shmbuffer); 
-  segment_size  =               shmbuffer.shm_segsz; 
-  printf ("segment size: %d\n", segment_size); 
-  /* Write a string to the shared memory segment.  */ 
-  //sprintf (shared_memory, "test!!!!."); 
-
 }
