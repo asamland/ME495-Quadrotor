c97a073f907890776c95fce21df09d321dcd75c1
Alex Samland <alexandersamland2020@u.northwestern.edu>
2019-02-07 22:20:10 -0600

    starting to add Thrust

--- week1_student.cpp
+++ week1_student.cpp
@@ -523,6 +523,7 @@ void safety_fail(){
 
 void pid_update(){
   Keyboard keyboard=*shared_memory;
+  int thrust = NEUTRAL_PWM+Thrust;
   static float pitch_previous = 0;
   static float i_pitch_error = 0;
   static float i_max = 100;
@@ -544,10 +545,10 @@ void pid_update(){
     i_pitch_error = -i_max;
   }
 
-  motor0PWM = keyboard.thrust - pitch_error*P - dpitch*D - i_pitch_error;
-  motor1PWM = keyboard.thrust + pitch_error*P + dpitch*D + i_pitch_error;
-  motor2PWM = keyboard.thrust - pitch_error*P - dpitch*D - i_pitch_error;
-  motor3PWM = keyboard.thrust + pitch_error*P + dpitch*D + i_pitch_error;
+  motor0PWM = Thrust - pitch_error*P - dpitch*D - i_pitch_error;
+  motor1PWM = Thrust + pitch_error*P + dpitch*D + i_pitch_error;
+  motor2PWM = Thrust - pitch_error*P - dpitch*D - i_pitch_error;
+  motor3PWM = Thrust + pitch_error*P + dpitch*D + i_pitch_error;
   set_PWM(0,motor0PWM);
   set_PWM(1,motor1PWM);
   set_PWM(2,motor2PWM);
